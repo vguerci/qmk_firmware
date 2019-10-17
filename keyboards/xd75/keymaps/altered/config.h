6d85840c636144660e85348821c3c7f8a13f4b51
Vincent Guerci <vincent@guerci.com>
2019-10-17 15:25:06 +0200

    xd75 preonic / altered mixup

---
+++ keyboards/xd75/keymaps/altered/config.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifdef AUDIO_ENABLE
+#define STARTUP_SONG SONG(PLANCK_SOUND)
+
+#define MIDI_BASIC
+
+#define ENCODER_RESOLUTION 4
+#endif
+
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#define NO_AUTO_SHIFT_ALPHA
+
+#define TAPPING_TERM 200
