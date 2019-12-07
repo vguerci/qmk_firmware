8d002b12faa3ddd66e75f3c85e9a5d9ba42a4696
Vincent Guerci <vguerci@gmail.com>
2019-12-07 16:36:30 +0100

    [altered] crkbd init

---
+++ keyboards/crkbd/keymaps/altered/rules.mk
@@ -0,0 +1,17 @@
+# RGBLIGHT_ENABLE = yes
+
+# RGBLIGHT_ENABLE = no
+# RGB_MATRIX_ENABLE = WS2812
+
+AUTO_SHIFT_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+
+# If you want to change the display of OLED, you need to change here
+SRC +=  ./lib/glcdfont.c \
+        ./lib/rgb_state_reader.c \
+        ./lib/layer_state_reader.c \
+        ./lib/logo_reader.c \
+        ./lib/keylogger.c \
+        # ./lib/mode_icon_reader.c \
+        # ./lib/host_led_state_reader.c \
+        # ./lib/timelogger.c \
