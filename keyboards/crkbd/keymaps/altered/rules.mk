c143b372609422591314fcf355976f990968cda8
Vincent Guerci <v.guerci@criteo.com>
2019-12-20 13:14:43 +0100

    [altered] media control keys

--- keyboards/crkbd/keymaps/altered/rules.mk
+++ keyboards/crkbd/keymaps/altered/rules.mk
@@ -7,6 +7,7 @@ MOUSEKEY_ENABLE = no
 
 AUTO_SHIFT_ENABLE = yes
 TAP_DANCE_ENABLE = yes
+EXTRAKEY_ENABLE = yes
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/glcdfont.c \
